4547bb3265ca7666716f8b3f23fd20e1e9a2d564
unknown <getsense@yandex.ru>
2023-09-24 21:06:16 +0300

    base start to main()

---
+++ boot_teapot/src/mbr_boot_sect.asm
@@ -0,0 +1,67 @@
+;MBR boot loader
+[org 0x7c00]
+
+	KERNEL_OFFSET equ 0x1000
+	KERNEL_SIZE equ 30
+
+	mov [BOOT_DRIVE], dl
+
+	mov bp, 0x9000
+	mov sp, bp
+
+	mov bx, MSG_REAL_MODE
+	call print_string
+
+	call load_kernel
+
+	call switch_to_pm
+
+	jmp $
+
+%include "print_string.asm"
+%include "disk_load.asm"
+%include "print_string_pm.asm"
+%include "gdt.asm"
+%include "switch_to_pm.asm"
+
+
+[bits 16]
+load_kernel:
+	mov bx, MSG_LOAD_KERNEL
+	call print_string
+
+	mov bx, KERNEL_OFFSET
+	mov dh, KERNEL_SIZE
+	mov dl, [BOOT_DRIVE]
+	call disk_load
+ret
+
+[bits 32]
+BEGIN_PM:
+	mov ebx, MSG_PROT_MODE
+	call print_string_pm
+
+	call KERNEL_OFFSET
+
+	jmp $
+
+
+	MSG_REAL_MODE:
+	db " Started in 16- bit Real Mode ", 0
+	
+	MSG_LOAD_KERNEL:
+	db " Loading kernel into memory. ", 0
+
+	MSG_PROT_MODE:
+	db " Successfully in 32- bit Protected Mode ", 0
+
+
+	BOOT_DRIVE:
+		db 0
+
+
+
+
+	times 510-($-$$) db 0
+
+dw 0xaa55
